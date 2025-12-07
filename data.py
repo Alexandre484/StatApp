7454f28f02ad8072a5d3451226797c60087044f9
Alexandre Toux <alexandre.toux@student-cs.fr>
2025-12-07 19:19:28 +0100

    premiere regression pour les urgences publiques

--- data.py
+++ data.py
@@ -1,3 +1,9 @@
 import numpy as np
 import pandas as pd
 
+MCO2022 = pd.read_csv("SAE/2022/MCO_2022r.csv", sep=";", encoding="latin-1")
+Urg2022 = pd.read_csv("SAE/2022/URGENCES_2022r.csv", sep=";", encoding="latin-1")
+Q202022 = pd.read_csv("SAE/2022/Q20_2022r.csv", sep=";", encoding="latin-1")
+
+FINESS = pd.read_excel("finess.xlsx")
+print(FINESS)
